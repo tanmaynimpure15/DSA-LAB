df7c70a6d7c7efe12cfb7994816b6e6583e42ee2
tanmaynimpure15 <tanmaygithub15@gmail.com>
2026-04-10 10:44:24 +0530

    Create  Singly Linked List Operations.c

---
+++ Singly Linked List Operations.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+struct LinkedList {
+    struct Node* head;
+    int size;
+};
+
+struct Node* createNode(int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    if (newNode == NULL) {
+        printf("Memory allocation failed\n");
+        exit(1);
+    }
+    newNode->data = data;
+    newNode->next = NULL;
+    return newNode;
+}
+
+struct LinkedList* initializeList() {
+    struct LinkedList* list = (struct LinkedList*)malloc(sizeof(struct LinkedList));
+    if (list == NULL) {
+        printf("Memory allocation failed\n");
+        exit(1);
+    }
+    list->head = NULL;
+    list->size = 0;
+    return list;
+}
+
+void insert(struct LinkedList* list, int data, int position) {
+// write your code here for insertion
+    
+    if (position < 0 || position > list->size) {
+        printf("Invalid position\n");
+        return;
+    }
+
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->next = NULL;
+
+    if (position == 0) {
+        newNode->next = list->head;
+        list->head = newNode;
+    } else {
+        struct Node* temp = list->head;
